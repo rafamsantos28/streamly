143adc8e6b7c4fd23066440ec59235cb0aaa0d09
rafamsantos28 <rafaelmsantos28@icloud.com>
2026-01-10 14:22:29 +0000

    Update Firebase configuration settings

--- firebase-config.js
+++ firebase-config.js
@@ -3,15 +3,16 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdKt3gpW556sKTFcs2rcFV-YZOW0tvoP4",
-  authDomain: "streamly-d3ea4.firebaseapp.com",
-  projectId: "streamly-d3ea4",
-  storageBucket: "streamly-d3ea4.firebasestorage.app",
-  messagingSenderId: "425019080684",
-  appId: "1:425019080684:web:af145c87eb027d72e7cac9",
-  measurementId: "G-PRZ6BZP088"
+  apiKey: "AIzaSyAkYudiGSZxhsiMj4MoLAMCLRErb9IlTDE",
+  authDomain: "streamly-87883.firebaseapp.com",
+  projectId: "streamly-87883",
+  storageBucket: "streamly-87883.firebasestorage.app",
+  messagingSenderId: "553778506908",
+  appId: "1:553778506908:web:bf4cc80e128038a3df9159",
+  measurementId: "G-DBL12V65JS"
 };
 
+// Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
